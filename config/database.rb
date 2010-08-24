c41cc84c9f8d35ad6d4f08d774cbcb6f3edbbc5a
Philip MacIver <philip.maciver@xing.com>
2010-08-24 16:17:34 +0200

    changed db config

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,7 @@
 DataMapper.logger = logger
 
 case Padrino.env
-  when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "tvrss_development.db"))
-  when :production  then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "tvrss_production.db"))
-  when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "tvrss_test.db"))
+  when :development then DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://" + Padrino.root('db', "tvrss_development.db"))
+  when :production  then DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://" + Padrino.root('db', "tvrss_production.db"))
+  when :test        then DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://" + Padrino.root('db', "tvrss_test.db"))
 end
