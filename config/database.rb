f5b51cd912ecf13e8c9f8c16d55f4f5d17c87cec
Philip MacIver <philip.maciver@xing.com>
2010-10-13 11:11:20 +0200

    changed sqlite to mysql

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,7 @@
 DataMapper.logger = logger
 
 case Padrino.env
-  when :development then DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://manman:don040609@localhost/ivercore_tvrss_dev")
-  when :production  then DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://manman:don040609@localhost/ivercore_tvrss_prod")
-  when :test        then DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://manman:don040609@localhost/ivercore_tvrss_test")
+  when :development then DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://macman:don040609@localhost/ivercore_tvrss_dev")
+  when :production  then DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://macman:don040609@localhost/ivercore_tvrss_prod")
+  when :test        then DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://macman:don040609@localhost/ivercore_tvrss_test")
 end
