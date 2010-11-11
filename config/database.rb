c24fef79a7e56f986059dc351764512331b75853
Philip MacIver <philip.maciver@xing.com>
2010-11-11 11:45:44 +0100

    changed the db to postgress

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,8 @@
 DataMapper.logger = logger
 
 case Padrino.env
-  when :development then DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://macman:don040609@localhost/tvrss_ivercore_dev")
-  when :production  then DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://macman:don040609@localhost/tvrss_ivercore_prod")
-  when :test        then DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://macman:don040609@localhost/tvrss_ivercore_test")
+  when :test        then DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://postgres:@localhost/tvrss_ivercore_test")
+  when :development then DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://postgres:@localhost/tvrss_ivercore_dev")
+  when :production  then DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://postgres:@localhost/tvrss_ivercore")
 end
+
