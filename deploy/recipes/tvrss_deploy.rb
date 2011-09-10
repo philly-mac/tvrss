7e788ebf5eb9010fb792de754eaaf8722fe198c8
Philip MacIver <philip.maciver@xing.com>
2011-09-10 05:04:35 +0200

    Updated readme

--- deploy/recipes/tvrss_deploy.rb
+++ deploy/recipes/tvrss_deploy.rb
@@ -1,10 +1,11 @@
 require 'deploy'
+require 'deploy/recipes/rails_data_mapper'
 
-class TvrssDeploy < Deploy::Recipes::RailsDataMapper
+class TvrssDeploy < ::Deploy::Recipes::RailsDataMapper
 
-  self.actions << :link_credentials
+  append  :link_credentials
 
-  def self.link_credentials
+  desc "link_credentials", "Links customs credentials file" do
     file_path = "#{Deploy::Config.get(:current_path)}/config/credentials.rb"
     file_exists(file_path, [ "rm #{file_path}" ])
     remote "ln -s #{Deploy::Config.get(:shared_path)}/credentials.rb #{Deploy::Config.get(:current_path)}/config/credentials.rb"
