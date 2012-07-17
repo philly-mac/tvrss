248d664a0a5102297e48d699afc65c19dd489fe9
Philip MacIver <philip@ivercore.com>
2012-07-17 17:54:34 +0200

    5 retries if request fails, and mass simplification of the code

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -15,92 +15,87 @@ class Episode
   property :watched,        Boolean, :default => false
   timestamps :at
 
-  class << self
-    def pad_num(num)
-      ret = '00'
-      s = /(\d+)/.match(num)
-      if s
-        s = s[0].to_i
-        s = "0#{s}" if s.to_s.size == 1
-        ret = s
-      end
-      ret
+  def self.pad_num(num)
+    ret = '00'
+    s = /(\d+)/.match(num)
+    if s
+      s = s[0].to_i
+      s = "0#{s}" if s.to_s.size == 1
+      ret = s
     end
+    ret
+  end
 
-    def import_episodes(force = false, tvr_show_id = nil)
-      requests = []
-      success = true
+  def self.populate_episodes(show, xml_document)
+    Episode.all(:show_id => shows.map(&:id)).destroy
 
-      shows = tvr_show_id ? Show.all(:tvr_show_id => tvr_show_id) : Show.all
+    xml_document.css('Show Episodelist Season').each do |season|
+      season.css("episode").each do |episode|
+        epnum     = episode.at_css("epnum").content
+        season_no = season['no']
+        seasonnum = episode.at_css("seasonnum").content
+        prodnum   = episode.at_css("prodnum").content
+        airdate   = Date.parse(episode.at_css("airdate").content) rescue nil
+        link      = episode.at_css("link").content
+        title     = episode.at_css("title").content
 
-      Episode.all(:show_id => shows.map(&:id)).destroy
+        episode = Episode.new({
+          :season         => season_no,
+          :episode        => epnum,
+          :season_episode => seasonnum,
+          :product_number => prodnum,
+          :url            => link,
+          :air_date       => airdate,
+          :title          => title,
+          :tvr_show_id    => show.tvr_show_id
+        })
 
-      hydra = Typhoeus::Hydra.new
+        show.episodes << episode
 
-      # Set up the requests
-      Rails.logger.info "Setting up requests"
-      shows.each do |show|
-        requests << Typhoeus::Request.new("http://services.tvrage.com/feeds/episode_list.php?sid=#{show.tvr_show_id}", {:timeout => 600000})
-        requests.last.on_complete do |response|
-          if response.success?
-            [show, Nokogiri::XML(response.body)]
-          else
-            [nil, nil]
-          end
-        end
-      end
-
-      # Queue each of the reuests
-      Rails.logger.info "Queuing up requests"
-      requests.each{|requests| hydra.queue requests}
-
-      # Run them
-      Rails.logger.info "Running up requests"
-      hydra.run
-
-      requests.each do|request|
-        show, xml_document = request.handled_response
-
-        if show
-
-          xml_document.css('Show Episodelist Season').each do |season|
-            season.css("episode").each do |episode|
-              epnum     = episode.at_css("epnum").content
-              season_no = season['no']
-              seasonnum = episode.at_css("seasonnum").content
-              prodnum   = episode.at_css("prodnum").content
-              airdate   = Date.parse(episode.at_css("airdate").content) rescue nil
-              link      = episode.at_css("link").content
-              title     = episode.at_css("title").content
-
-              episode = Episode.new({
-                :season         => season_no,
-                :episode        => epnum,
-                :season_episode => seasonnum,
-                :product_number => prodnum,
-                :url            => link,
-                :air_date       => airdate,
-                :title          => title,
-                :tvr_show_id    => show.tvr_show_id
-              })
-
-              show.episodes << episode
-
-              if episode.save
-                Rails.logger.info "new episode saved #{episode.id}"
-              else
-                Rails.logger.error "Failed to saved #{episode.errors.inspect}"
-              end
-            end
-          end
-
+        if episode.save
+          Rails.logger.info "new episode saved #{episode.id}"
         else
-          success = false
+          Rails.logger.error "Failed to saved #{episode.errors.inspect}"
         end
       end
-
-      success
     end
   end
+
+  def self.import_episodes(tvr_show_id = nil)
+    counter = 0
+    failed_shows = []
+
+    shows = tvr_show_id ? Show.all(:tvr_show_id => tvr_show_id) : Show.all
+
+    shows.each do |show|
+      counter = 0
+
+      while counter < 5
+        Rails.logger.info "Try #{counter + 1} to get #{show.name}"
+        response = Typhoeus::Request.get(Typhoeus::Request.new("http://services.tvrage.com/feeds/episode_list.php?sid=#{show.tvr_show_id}", {:timeout => 600000}))
+
+        if response.success?
+          Rails.logger.info "#{show.name} success"
+          populate_episodes(show, Nokogiri::XML(response.body))
+          break
+        else
+          Rails.logger.info "HTTP request failed: " + response.code.to_s
+          Rails.logger.info "#{show.name} failure.."
+
+          counter += 1
+
+          if counter < 5
+            Rails.logger.info "#{show.name} trying again"
+          else
+            failed_shows << show.name
+          end
+
+        end
+      end
+    end
+
+    failed_shows
+  end
+
 end
 
